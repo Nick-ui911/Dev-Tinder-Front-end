25f67a8c9c726c5ac52b666964282bcb453371ab
Nick-ui911 <nbaghel392@gmail.com>
2025-01-29 22:30:54 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import React from "react";
+import { BrowserRouter, Routes ,Route} from "react-router-dom";
+import Home from "./components/Home";
 
-
-function App() {
-
-
+const App = () => {
   return (
     <>
-    <h1>hello world</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
