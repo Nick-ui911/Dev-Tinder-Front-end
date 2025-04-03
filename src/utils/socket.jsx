eb00ebb78c6bab48db6fbb64660c0635cb1ebf6f
Nick-ui911 <nbaghel392@gmail.com>
2025-04-03 14:08:56 +0530

    .

--- src/utils/socket.jsx
+++ src/utils/socket.jsx
@@ -1,17 +1,9 @@
 import { io } from "socket.io-client";
 import { BASE_URL } from "./constants";
-
 export const createSocketConnection = () => {
   if (location.hostname === "localhost") {
-    return io(BASE_URL, {
-      transports: ["websocket", "polling"], // Allow both
-      withCredentials: true,
-    });
+    return io(BASE_URL);
   } else {
-    return io("/", {
-      path: "/api/socket.io",
-      transports: ["websocket", "polling"],
-      withCredentials: true,
-    });
+    return io("/", { path: "/api/socket.io" });
   }
 };
